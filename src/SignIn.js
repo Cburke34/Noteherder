441b1cee209327cd0fc0c15cffcfed85ad30bd4e
Chris Burke <burkech@umail.iu.edu>
2017-07-11 00:39:57 -0500

    Created GitHub login ability

--- src/SignIn.js
+++ src/SignIn.js
@@ -6,8 +6,15 @@ import googleLogo from './google.svg'
 import { auth, googleProvider, gitProvider } from './base'
 
 const SignIn = () => {
-  const authenticate = () => {
+  
+  const authenticateGit = () => {
+    auth.signInWithPopup(gitProvider)
+
+  }
+
+  const authenticateGoogle = () => {
     auth.signInWithPopup(googleProvider)
+    
   }
 
   return (
@@ -19,14 +26,24 @@ const SignIn = () => {
       <main>
         <h3>Hey, Nerd! You Like Notes?</h3>
         <p>You never know when you'll need to write crap down. In fact, you should probably be taking notes right now.</p>
-        
+
         <button
           className="google"
-          onClick={authenticate}
+          onClick= {authenticateGoogle}
         >
-          <img src={googleLogo} alt="" />
+        <img src={googleLogo} alt="" />
           Sign in with Google
         </button>
+
+        <button 
+          className="github"
+          onClick= {authenticateGit}
+        >
+        <i className="fa fa-github"></i>
+          Sign in with GitHub
+        </button>
+
+
       </main>
     </div>
   )
